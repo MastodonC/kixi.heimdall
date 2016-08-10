a02e3655067d30b0b1d3ecd6e79a770cf9275fa1
Elise Huard <elise@mastodonc.com>
2016-08-10 15:12:14 +0100

    centralizes some configuration parameters for cassandra

--- src/kixi/heimdall/system.clj
+++ src/kixi/heimdall/system.clj
@@ -9,13 +9,15 @@
 (defn system []
   (let [api-port 3000
         cassandra-host "localhost"
+        cassandra-keyspace "heimdall"
+        cassandra-replication-factor 1
         profile :development]
     (-> (component/system-map
          :jetty-server (->JettyServer #'kixi.heimdall.handler/app api-port)
          :repl-server  (Object.) ; dummy - replaced when invoked via uberjar.
          :cluster (new-cluster {})
          :cassandra-session (new-session {:host cassandra-host
-                                          :keyspace "heimdall"
-                                          :replication-factor 1} profile))
+                                          :keyspace cassandra-keyspace
+                                          :replication-factor cassandra-replication-factor} profile))
         (component/system-using
          {:cassandra-session [:cluster]}))))
