38fd39b49cdbeec53d94cae0ea6211e7a422eb60
Elise Huard <elise@mastodonc.com>
2016-08-15 15:46:04 +0100

    pem files for test purposes

--- test/kixi/heimdall/handler_test.clj
+++ test/kixi/heimdall/handler_test.clj
@@ -3,9 +3,13 @@
             [ring.mock.request :as mock]
             [kixi.heimdall.handler :refer :all]
             [kixi.heimdall.user :as user]
-            [buddy.hashers :as hs]))
+            [buddy.hashers :as hs]
+            [clojure.java.io :as io]
+            [clojure.edn :as edn]))
 
-
+(def auth-config-test
+  (let [f (io/resource "auth-conf-test.edn")]
+    (edn/read-string (slurp f))))
 
 (deftest test-app
   (testing "main route"
@@ -18,20 +22,21 @@
       (is (= (:status response) 404))))
 
   (testing "auth route"
-    (testing "authentication succeeds"
-      (with-redefs [user/find-by-username (fn [session m] {:username "user" :password (hs/encrypt "foo")})]
-        (let [response (app (mock/request :post "/create-auth-token"
-                                          {:username "user" :password "foo"}))]
-          (is (= (:status response) 201))
-          (is (:token (:body response))))))
-    (testing "authentication fails wrong user"
-      (with-redefs [user/find-by-username (fn [session m] nil)]
-        (let [response (app (mock/request :post "/create-auth-token"
-                                          {:username "user" :password "foo"}))]
-          (is (= (:status response) 401)))))
-    (testing "authentication fails wrong pass"
-      (with-redefs [user/find-by-username (fn [session m] {:username "user" :password (hs/encrypt "foobar")})]
-        (let [response (app (mock/request :post "/create-auth-token"
-                                          {:username "user" :password "foo"}))]
-          (is (= (:status response) 401))))))
+    (with-redefs [auth-config auth-config-test]
+      (testing "authentication succeeds"
+        (with-redefs [user/find-by-username (fn [session m] {:username "user" :password (hs/encrypt "foo")})]
+          (let [response (app (mock/request :post "/create-auth-token"
+                                            {:username "user" :password "foo"}))]
+            (is (= (:status response) 201))
+            (is (:token (:body response))))))
+      (testing "authentication fails wrong user"
+        (with-redefs [user/find-by-username (fn [session m] nil)]
+          (let [response (app (mock/request :post "/create-auth-token"
+                                            {:username "user" :password "foo"}))]
+            (is (= (:status response) 401)))))
+      (testing "authentication fails wrong pass"
+        (with-redefs [user/find-by-username (fn [session m] {:username "user" :password (hs/encrypt "foobar")})]
+          (let [response (app (mock/request :post "/create-auth-token"
+                                            {:username "user" :password "foo"}))]
+            (is (= (:status response) 401)))))))
   )
