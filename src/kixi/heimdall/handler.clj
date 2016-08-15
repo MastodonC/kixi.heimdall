8f3af397008c2dfdbe1d949ac208402bdf2eab1c
Eleonore Mayola <eleonore.mayola@ymail.com>
2016-08-15 14:03:49 +0100

    Create key, wip: create signed token

--- src/kixi/heimdall/handler.clj
+++ src/kixi/heimdall/handler.clj
@@ -4,12 +4,45 @@
             [ring.middleware.defaults :refer [wrap-defaults api-defaults]]
             [kixi.heimdall.application :as app]
             [taoensso.timbre :as log]
-            [kixi.heimdall.components.database :as db]            ))
+            [kixi.heimdall.components.database :as db]
+            [buddy.sign.jws :as jws]
+            [buddy.sign.util :as sign]
+            [buddy.core.keys :as ks]
+            [clj-time.core :as t]
+            [clojure.java.io :as io]
+            [kixi.heimdall.user :as user]))
+
+(defn- pkey [auth-conf]
+  (ks/private-key
+   (io/resource (:privkey auth-conf))
+   (:passphrase auth-conf)))
+
+(defn create-auth-token [session auth-conf credentials]
+  (let [[ok? res] (user/auth session credentials)
+        exp (-> (t/plus (t/now) (t/days 1)) (sign/to-timestamp))]
+    (if ok?
+      [true {:token (jws/sign res
+                              (pkey auth-conf)
+                              {:alg :rs256 :exp exp})}]
+      [false res])))
+
+(defn auth-token [session auth-conf params]
+  (let [[ok? res] (create-auth-token session
+                                     auth-conf
+                                     params)]
+    (println params)
+    (if ok?
+      {:status 201 :body res}
+      {:status 401 :body res})))
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
   (GET "/database" [] (let [session (:cassandra-session app/system)]
                         (db/select session "users" "username" {:password "boo" :username "user"})))
+  (POST "/create-auth-token" [& params] (let [session (:cassandra-session app/system)
+                                         auth-conf {:privkey "auth_privkey.pem"
+                                                    :passphrase "bigdata"}]
+                                     (auth-token session auth-conf params)))
   (route/not-found "Not Found"))
 
 (defn wrap-catch-exceptions [handler]
--- test/kixi/heimdall/handler_test.clj
+++ test/kixi/heimdall/handler_test.clj
@@ -1,7 +1,11 @@
 (ns kixi.heimdall.handler-test
   (:require [clojure.test :refer :all]
             [ring.mock.request :as mock]
-            [kixi.heimdall.handler :refer :all]))
+            [kixi.heimdall.handler :refer :all]
+            [kixi.heimdall.user :as user]
+            [buddy.hashers :as hs]))
+
+
 
 (deftest test-app
   (testing "main route"
@@ -11,4 +15,10 @@
 
   (testing "not-found route"
     (let [response (app (mock/request :get "/invalid"))]
-      (is (= (:status response) 404)))))
+      (is (= (:status response) 404))))
+
+  (testing "auth route"
+    (with-redefs [user/find-by-username (fn [session m] {:username "user" :password (hs/encrypt "foo")})]
+      (let [response (app (mock/request :post "/create-auth-token"
+                                        {:username "user" :password "foo"}))]
+        (is (= (:status response) 201))))))
