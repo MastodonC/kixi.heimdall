e8d7776c640bbc4608dc6a88623e0fe5debabc86
Eleonore Mayola <eleonore.mayola@ymail.com>
2016-08-31 14:20:23 +0100

    Returns 401 when the (un)sign fails.

--- src/kixi/heimdall/service.clj
+++ src/kixi/heimdall/service.clj
@@ -32,11 +32,12 @@
 
 (defn- make-auth-token [user auth-conf]
   (let [exp (-> (t/plus (t/now) (t/minutes 30)) (c/to-long))]
-    (try (jwt/sign user
-                   (private-key auth-conf)
-                   {:alg :rs256 :exp exp})
-         (catch Exception _ (do (log/debug "Sign auth token failed")
-                                nil)))))
+    (if user (try (jwt/sign user
+                              (private-key auth-conf)
+                              {:alg :rs256 :exp exp})
+                    (catch Exception _ (do (log/debug "Sign auth token failed")
+                                           nil)))
+        (do (log/debug "User credentials missing") nil))))
 
 (defn unsign-token [auth-conf token]
   (and token
@@ -48,25 +49,29 @@
 
 (defn make-refresh-token [issued-at-time auth-conf user]
   (let [exp (-> (t/plus (t/now) (t/days 30)) (c/to-long))]
-    (try (jwt/sign {:user-id (:id user)}
-                   (private-key auth-conf)
-                   {:alg :rs256 :iat issued-at-time :exp exp})
-         (catch Exception _ (do (log/debug "Sign refresh token failed")
-                                nil)))))
+    (if user (try (jwt/sign {:user-id (:id user)}
+                            (private-key auth-conf)
+                            {:alg :rs256 :iat issued-at-time :exp exp})
+                  (catch Exception _ (do (log/debug "Sign refresh token failed")
+                                         nil)))
+        (do (log/debug "User credentials missing") nil))))
 
 (defn make-token-pair! [session auth-conf user]
   (let [issued-at-time (c/to-long (t/now))
-        refresh-token (make-refresh-token issued-at-time auth-conf user)]
-    (refresh-token/add! session {:refresh-token refresh-token
-                                 :issued issued-at-time
-                                 :user-id (:id user)})
-    {:token-pair {:auth-token (make-auth-token user auth-conf)
-                  :refresh-token refresh-token}}))
+        refresh-token (make-refresh-token issued-at-time auth-conf user)
+        auth-token (make-auth-token user auth-conf)]
+    (when (and refresh-token auth-conf)
+      (do (refresh-token/add! session {:refresh-token refresh-token
+                                       :issued issued-at-time
+                                       :user-id (:id user)})
+          {:token-pair {:auth-token auth-token
+                        :refresh-token refresh-token}}))))
 
 (defn create-auth-token [session auth-conf credentials]
-  (let [[ok? res] (user/auth session credentials)]
-    (if ok?
-      (success (make-token-pair! session auth-conf (:user res)))
+  (let [[ok? res] (user/auth session credentials)
+        token-pair (make-token-pair! session auth-conf (:user res))]
+    (if (and ok? token-pair)
+      (success token-pair)
       (fail "Invalid username or password"))))
 
 (defn refresh-auth-token [session auth-conf refresh-token]
@@ -75,11 +80,12 @@
           refresh-token-data (refresh-token/find-by-user-and-issued session
                                                                     user-uuid
                                                                     (:iat unsigned))
-          user (user/find-by-id session user-uuid)]
-      (if (:valid refresh-token-data)
+          user (user/find-by-id session user-uuid)
+          new-token-pair (make-token-pair! session auth-conf user)]
+      (if (and (:valid refresh-token-data) new-token-pair)
         (do
           (refresh-token/invalidate! session (:id refresh-token-data))
-          (success  (make-token-pair! session auth-conf user)))
+          (success new-token-pair))
         (fail "Refresh token revoked/deleted or new refresh token already created")))
     (fail "Invalid or expired refresh token provided")))
 
