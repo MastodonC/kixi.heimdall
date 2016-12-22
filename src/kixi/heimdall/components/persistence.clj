77e97fdf0872ed6187ed4ed409c22ca604144b0b
Tom Coupland <tcoupland@gmail.com>
2016-12-22 14:49:43 +0000

    Ensuring persist event consumption doesn't produce anything

--- src/kixi/heimdall/components/persistence.clj
+++ src/kixi/heimdall/components/persistence.clj
@@ -13,17 +13,17 @@
                                     :kixi.heimdall/persistence-group-created
                                     :kixi.heimdall/group-created
                                     "1.0.0"
-                                    (comp (partial #'service/create-group cassandra-session) :kixi.comms.event/payload))
+                                    (comp (constantly nil) (partial #'service/create-group cassandra-session) :kixi.comms.event/payload))
            (c/attach-event-handler! communications
                                     :kixi.heimdall/persistence-member-added
                                     :kixi.heimdall/member-added
                                     "1.0.0"
-                                    (comp (partial #'service/add-member cassandra-session) :kixi.comms.event/payload))
+                                    (comp (constantly nil) (partial #'service/add-member cassandra-session) :kixi.comms.event/payload))
            (c/attach-event-handler! communications
                                     :kixi.heimdall/persistence-member-removed
                                     :kixi.heimdall/member-removed
                                     "1.0.0"
-                                    (comp (partial #'service/remove-member cassandra-session) :kixi.comms.event/payload))]]
+                                    (comp (constantly nil) (partial #'service/remove-member cassandra-session) :kixi.comms.event/payload))]]
       (assoc component :event-handlers event-handlers)))
   (stop [{:keys [communications event-handlers] :as component}]
     (doseq [handler event-handlers] (c/detach-handler! communications handler))
