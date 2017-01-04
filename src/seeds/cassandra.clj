281f9f8aa5e8a9fd7ec23de07306eb092b954a39
Antony Woods <antony@mastodonc.com>
2017-01-04 09:29:02 +0000

    Add a test group

--- src/seeds/cassandra.clj
+++ src/seeds/cassandra.clj
@@ -3,8 +3,7 @@
             [qbits.hayt                 :as hayt]
             [joplin.cassandra.database :refer [get-connection]]
             [kixi.heimdall.user :as user]
-            [kixi.heimdall.group :as group]
-            [kixi.heimdall.member :as member]
+            [kixi.heimdall.service :as service]
             [kixi.heimdall.components.database :as db]))
 
 (defn run-dev [target & args]
@@ -14,12 +13,11 @@
         ;; Add a test user
         test-user (user/add! dc {:username "test@mastodonc.com"
                                  :name     "Test User"
-                                 :password "Secret123"})
-        ;; Add a test group
-        test-group (group/create! dc {:name "Test Group"
-                                      :user-id (:id test-user)})
-        ;; Add test user to test group
-        _ (member/add-user-to-group dc (:id test-user) (:group-id test-group))]))
+                                 :password "Secret123"})]
+    ;; Add a test group
+    (#'service/create-group dc
+                            {:group {:group-name "Test Group"}
+                             :user {:id (str (:id test-user))}})))
 
 (defn run-prod [target & args]
   (let [conn (get-connection (-> target :db :hosts)
