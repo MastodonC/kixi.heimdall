470c4e2668dca3e0446763f1c0dda53b6267c89d
Elise Huard <elise@mastodonc.com>
2017-01-16 17:18:15 +0000

    add possibility to vary instances

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,11 +6,12 @@ SEBASTOPOL_IP=$1
 ENVIRONMENT=$2
 IMAGE_ARG=$3
 IMAGE=${IMAGE_ARG?mastodonc/kixi.heimdall}
+INSTANCES=1
 
 # using deployment service sebastopol
 TAG=git-$(echo $CIRCLE_SHA1 | cut -c1-12)
 VPC=sandpit
-sed -e "s/@@TAG@@/$TAG/" -e "s/@@ENVIRONMENT@@/$ENVIRONMENT/" -e "s/@@VPC@@/$VPC/" -e "s|@@IMAGE@@|$IMAGE|"  $APP_NAME.json.template > $APP_NAME.json
+sed -e "s/@@TAG@@/$TAG/" -e "s/@@ENVIRONMENT@@/$ENVIRONMENT/" -e "s/@@VPC@@/$VPC/" -e "s/@@INSTANCES@@/$INSTANCES/" -e "s|@@IMAGE@@|$IMAGE|"  $APP_NAME.json.template > $APP_NAME.json
 
 # we want curl to output something we can use to indicate success/failure
 STATUS=$(curl -s -o /dev/null -w "%{http_code}" -X POST http://$SEBASTOPOL_IP:9501/marathon/$APP_NAME -H "Content-Type: application/json" -H "$SEKRIT_HEADER: 123" --data-binary "@$APP_NAME.json")
