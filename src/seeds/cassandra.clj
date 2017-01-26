5c9a9dda2ef5de85724da77ef523c55c09dbb0fd
Antony Woods <antony@mastodonc.com>
2017-01-26 10:47:22 +0000

    More comprehensive seed for staging

--- src/seeds/cassandra.clj
+++ src/seeds/cassandra.clj
@@ -19,21 +19,65 @@
     ;; Add a test group
     (#'service/create-group dc
                             {:group {:group-name "Test Group"}
-                             :user-id (str (:id test-user))})))
+                             :user-id (str (:id test-))})))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(def staging-users
+  {:test "Test User"
+   :antony "Antony"
+   :tom "Tom"
+   :elise "Elise"
+   :bruce "Bruce"
+   :sam "Sam"
+   :seb "Seb"
+   :sunny "Sunny"
+   :eleonore "Eleonore"
+   :fran "Fran"
+   :chris "Chris"
+   :jase "Jase"})
+
+(def staging-groups
+  {"Dev Team" [:antony :tom :elise]
+   "Product Team" [:sam :bruce]
+   "Cool Kids" [:seb :sunny :eleonore :jase]
+   "Sales Team" [:fran :chris]})
+
+(defn create-user!
+  [dc [k user-name]]
+  (let [email (str (name k)"@mastodonc.com")
+        existing (user/find-by-username session {:username email})]
+    (if existing
+      existing
+      (let [u (user/add! dc {:username email
+                             :password "Secret123"
+                             :name user-name})]
+        (service/add-self-group dc u)
+        u))))
+
+(defm create-group!
+  [dc all-users [group-name users]]
+  (let [group (#'service/create-group dc
+                                      {:group {:group-name group-name}
+                                       :user-id (str (:id (get all-users (first users))))})]
+    (run! (fn [user]
+            (#'service/add-member dc
+                                  {:group-id (:group-id group)
+                                   :user-id (str (:id (get all-users user)))}))
+          (rest users))))
 
 (defn run-staging [target & args]
   (let [conn (get-connection (-> target :db :hosts)
                              (-> target :db :keyspace))
         dc (db/->DirectConnection {:session conn})
-        ;; Add a test user
-        test-user (user/add! dc {:username "test@mastodonc.com" :password "Secret123" :name "Test User"})
-        _ (group/add! dc {:name "Test User"
-                          :user-id (:id test-user)
-                          :group-type "user"})]
-    ;; Add a test group
-    (#'service/create-group dc
-                            {:group {:group-name "Test Group"}
-                             :user-id (str (:id test-user))})))
+        ;; Add test users
+        users (zipmap (keys staging-users)
+                      (map (partial create-users! dc) staging-users))
+
+        ;; Add test groups
+        groups (zipmap (keys staging-groups)
+                       (map (partial create-group! dc users) staging-groups))]))
 
 (defn run-prod [target & args]
   (let [conn (get-connection (-> target :db :hosts)
