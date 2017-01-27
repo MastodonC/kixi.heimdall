104b3a69e03d03156005d58c400dddd128200938
Tom Coupland <tcoupland@gmail.com>
2017-01-27 17:50:19 +0000

    Adding HeapDump Path

--- scripts/run.sh
+++ scripts/run.sh
@@ -8,4 +8,4 @@ set -o xtrace
 
 SANDBOX=${MESOS_SANDBOX:-"."}
 
-exec java -XX:+HeapDumpOnOutOfMemoryError -XX:+UseG1GC -Xloggc:$SANDBOX/gc.log -XX:+PrintGCCause -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=3 -XX:GCLogFileSize=2M ${JAVA_OPTS:-} -jar /srv/kixi.heimdall.jar --profile $PROFILE
+exec java -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=$SANDBOX -XX:+UseG1GC -Xloggc:$SANDBOX/gc.log -XX:+PrintGCCause -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=3 -XX:GCLogFileSize=2M ${JAVA_OPTS:-} -jar /srv/kixi.heimdall.jar --profile $PROFILE
