c7078739e9881205544595e8ec0fa4560a5fc3ae
Antony Woods <antony@mastodonc.com>
2017-04-24 15:43:17 +0100

    WIP

---
+++ src/migrators/dynamodb/20170424153900_invites.clj
@@ -0,0 +1,25 @@
+(ns migrators.dynamodb.20170424153900-invites
+  (:require [kixi.heimdall.components.database :as db]
+            [kixi.heimdall.config :as config]
+            [kixi.heimdall.application :as app]
+            [taoensso.faraday :as far]
+            [taoensso.timbre :as log]))
+
+(defn get-db-config
+  []
+  (let [conf (config/config @app/profile)]
+    (:dynamodb conf)))
+
+(defn up
+  [db]
+  (let [conn (db/new-session (get-db-config) @app/profile)]
+    (db/create-table conn
+                     "invites"
+                     [:code :s]
+                     {:throughput {:read 1 :write 1}
+                      :block? true})))
+
+(defn down
+  [db]
+  (let [conn (db/new-session (get-db-config) @app/profile)]
+    (db/delete-table conn "invites")))
