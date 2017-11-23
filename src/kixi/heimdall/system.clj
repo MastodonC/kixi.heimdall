0c9516c774cbad296467bdfe79b35a41fa0597c7
Tom Coupland <tcoupland@gmail.com>
2017-11-23 15:05:16 +0000

    Reset config-location

--- src/kixi/heimdall/system.clj
+++ src/kixi/heimdall/system.clj
@@ -18,10 +18,11 @@
 
 (defn system
   ([profile]
-    (system @app/config-location profile))
+   (system @app/config-location profile))
   ([config-location profile]
    (let [config (config/config config-location profile)
-         _ (reset! app/profile profile)]
+         _ (reset! app/profile profile)
+         _ (reset! app/config-location config-location)]
      (log/set-config! {:level          (keyword (env :log-level (get-in config [:logging :level])))
                        :timestamp-opts kixi-log/default-timestamp-opts
                        :appenders      (if (#{:prod :staging :staging-replay} profile)
@@ -32,16 +33,16 @@
        (comms/set-verbose-logging! true))
      (log/info "System with" profile)
      (-> (component/system-map
-           :metrics (metrics/map->Metrics (:metrics config))
-           :web-server (web/new-http-server (config/webserver-port config) config)
-           :repl-server (Object.)                           ; dummy - replaced when invoked via uberjar.
-           :db (db/new-session (:dynamodb config) profile)
-           :communications (case (first (keys (:communications config)))
-                             :kinesis (kinesis/map->Kinesis (:kinesis (:communications config)))
-                             :coreasync (coreasync/map->CoreAsync (:coreasync (:communications config))))
-           :persistence (persistence/->Persistence)
-           :commands (commands/->Commands))
+          :metrics (metrics/map->Metrics (:metrics config))
+          :web-server (web/new-http-server (config/webserver-port config) config)
+          :repl-server (Object.)                           ; dummy - replaced when invoked via uberjar.
+          :db (db/new-session (:dynamodb config) profile)
+          :communications (case (first (keys (:communications config)))
+                            :kinesis (kinesis/map->Kinesis (:kinesis (:communications config)))
+                            :coreasync (coreasync/map->CoreAsync (:coreasync (:communications config))))
+          :persistence (persistence/->Persistence)
+          :commands (commands/->Commands))
          (component/system-using
-           {:web-server  [:metrics :communications :db]
-            :persistence [:communications :db]
-            :commands    [:communications :db]})))))
+          {:web-server  [:metrics :communications :db]
+           :persistence [:communications :db]
+           :commands    [:communications :db]})))))
