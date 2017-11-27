7cbdcb82e09bc36055fc105467f75b9826505f85
Matt Ford <matt@dancingfrog.co.uk>
2017-11-27 14:35:34 +0000

    Migrate user table :pre-signup key to a bool (from a nippy encoded bool)

---
+++ src/migrators/dynamodb/20171127120500_presignup_bool_type_change.clj
@@ -0,0 +1,27 @@
+(ns migrators.dynamodb.20171127120500_presignup_bool_type_change
+  (:require [kixi.heimdall.components.database :as db]
+            [kixi.heimdall.config :as config]
+            [kixi.heimdall.application :as app]
+            [taoensso.faraday :as far]))
+
+(defn get-db-config
+  []
+  (let [conf (config/config @app/profile)]
+    (:dynamodb conf)))
+
+(defn up
+  [db]
+  (let [conn (db/new-session (get-db-config) @app/profile)
+        users (db/scan conn "users")
+        ;; we read the nippy encoded value out of the pre-signup field (which is automatically typed to a bool)
+        ;; then we write it back as an actual bool rather than the nippy format.
+        pre-signup-type-fn (fn [{:keys [pre-signup id]}]
+                             (db/update-item conn
+                                             "users"
+                                             {:id id}
+                                             {:update-expr    "SET pre-signup = :v"
+                                              :expr-attr-vals {":v" pre-signup}}))]
+    (run! pre-signup-type-fn users)))
+
+(defn down
+  [db])
