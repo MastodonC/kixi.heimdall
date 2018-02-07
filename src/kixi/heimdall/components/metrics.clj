930016cf975ec28369e7ce349760af7b2f9f5bad
Matt Ford <matt@dancingfrog.co.uk>
2018-02-07 17:16:52 +0000

    Revert "Metrics as per datastore"
    This reverts commit 5e75cc0

--- src/kixi/heimdall/components/metrics.clj
+++ src/kixi/heimdall/components/metrics.clj
@@ -1,8 +1,8 @@
 (ns kixi.heimdall.components.metrics
   (:require [clojure.string :as str]
             [com.stuartsierra.component :as component]
-            [kixi.metrics.name-safety :refer [safe-name]]
             [kixi.metrics.reporters.json-console :as reporter]
+            [kixi.metrics.name-safety :refer [safe-name]]
             [metrics
              [core :refer [new-registry]]
              [histograms :refer [histogram update!]]
@@ -10,61 +10,6 @@
             [metrics.jvm.core :as jvm]
             [metrics.ring.expose :as expose]
             [taoensso.timbre :as log]))
-
-(def upper-name
-  (memoize
-    (fn [level]
-      (str/upper-case (name level)))))
-
-(def uri-method-status->metric-name
-  (memoize
-    (fn [uri method status]
-      (let [name (-> (str "." (upper-name method))
-                     safe-name
-                     (str "." status))]
-        ["resources" (safe-name uri) name]))))
-
-(defn request-ctx->metric-name
-  [ctx]
-  (uri-method-status->metric-name
-    (get-in ctx [:request :uri])
-    (:method ctx)
-    (get-in ctx [:response :status])))
-
-(defn now
-  []
-  (System/currentTimeMillis))
-
-(defn append-rates
-  [metric-name]
-  (update metric-name 2
-          #(str % ".rates")))
-
-(defn insert-time
-  "Interceptor that stores the current time in the context under [:metrics ky]"
-  [ky]
-  (fn [ctx]
-    (assoc-in ctx [:metrics ky] (now))))
-
-(defn record-metrics
-  "Interceptor that records the time since [:metrics ky]"
-  [registry ky]
-  (fn [ctx]
-    (let [metric-name (request-ctx->metric-name ctx)
-          met (meter registry (append-rates metric-name))]
-      (when-let [start-time (get-in ctx [:metrics ky])]
-        ;On exception this won't get the modified context, i.e. the one with start time in it. This is due to the manifold chain and catch mechinism used.
-        (update! (histogram registry metric-name)
-                 (- (now) start-time)))
-      (mark! met))
-    ctx))
-
-(defn expose-metrics-resource
-  [registry]
-  {:methods
-   {:get
-    {:produces "application/json"
-     :response #(expose/render-metrics registry)}}})
 
 (defn meter-mark!
   [registry]
@@ -88,13 +33,7 @@
                                              (log/info "Starting JSON Metrics Reporter")
                                              (reporter/start reporter (:seconds json-reporter))
                                              reporter)))
-          (update :meter-mark #(or % (meter-mark! reg)))
-          (update :insert-time-in-ctx #(or %
-                                           (insert-time :resource)))
-          (update :expose-metrics-resource #(or %
-                                                (expose-metrics-resource reg)))
-          (update :record-ctx-metrics #(or %
-                                           (record-metrics reg :resource))))))
+          (update :meter-mark #(or % (meter-mark! reg))))))
   (stop [component]
     (-> component
         (update :json-reporter-inst #(when %
@@ -102,8 +41,6 @@
                                        (reporter/stop %)
                                        nil))
         (dissoc :meter-mark)
-        (dissoc :insert-time-in-ctx)
-        (dissoc :record-ctx-metrics)
         (update :registry #(when %
                              (log/info "Destroying metrics registry")
                              (.removeMatching % (com.codahale.metrics.MetricFilter/ALL))
