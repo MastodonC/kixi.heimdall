a9c1b64cab9d303a1fe68c8f98e30ca1564a96ea
Matt Ford <matt@dancingfrog.co.uk>
2018-02-27 15:18:39 +0000

    Bump log

--- project.clj
+++ project.clj
@@ -13,7 +13,7 @@
                  [ring/ring-jetty-adapter "1.5.0"]
                  [com.taoensso/timbre "4.8.0"]
                  [com.taoensso/faraday "1.9.0" :exclusions [com.amazonaws/aws-java-sdk-dynamodb]]
-                 [kixi/kixi.log "0.1.5"]
+                 [kixi/kixi.log "0.1.6"]
                  [kixi/kixi.metrics "0.4.1"]
                  [kixi/kixi.spec "0.1.18"]
                  [metrics-clojure ~metrics-version]
